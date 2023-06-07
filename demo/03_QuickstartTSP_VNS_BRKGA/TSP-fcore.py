2cd7022c4b20d2718597554d022f2312594f7613
Igor Machado <igor.machado@gmail.com>
2023-06-07 17:05:02 +0000

    feat: cleanup MoveSwapClass mess on tsp

--- demo/03_QuickstartTSP_VNS_BRKGA/TSP-fcore.py
+++ demo/03_QuickstartTSP_VNS_BRKGA/TSP-fcore.py
@@ -89,33 +89,6 @@ assert isinstance(ProblemContextTSP,  XMinimize)     # composition tests
 
 from optframe.components import Move
 
-'''
-# move
-class MoveSwap(object):
-    def __init__(self, _i: int = 0, _j: int = 0):
-        self.i = _i
-        self.j = _j
-    def __str__(self):
-        return "MoveSwap(i="+str(self.i)+";j="+str(self.j)+")"
-    @staticmethod
-    def apply(problemCtx: ProblemContextTSP, m: 'MoveSwap', sol: SolutionTSP) -> 'MoveSwap':
-        aux = sol.cities[m.j]
-        sol.cities[m.j] = sol.cities[m.i]
-        sol.cities[m.i] = aux
-        # must create reverse move (j,i)
-        return MoveSwap(m.j, m.i)
-    @staticmethod
-    def canBeApplied(problemCtx: ProblemContextTSP, m: 'MoveSwap', sol: SolutionTSP) -> bool:
-        return True
-    @staticmethod
-    def eq(problemCtx: ProblemContextTSP, m1: 'MoveSwap', m2: 'MoveSwap') -> bool:
-        return (m1.i == m2.i) and (m1.j == m2.j)
-    
-
-assert isinstance(MoveSwap, XMove)            # composition tests
-assert not(MoveSwap in Move.__subclasses__()) # staticmethod style
-'''
-
 class MoveSwapClass(Move):
     def __init__(self, _i: int = 0, _j: int = 0):
         self.i = _i
@@ -132,7 +105,6 @@ class MoveSwapClass(Move):
         return True
     def eq(self, problemCtx: ProblemContextTSP, m2: 'MoveSwapClass') -> bool:
         return (self.i == m2.i) and (self.j == m2.j)
-
 
 assert isinstance(MoveSwapClass, XMove)       # composition tests
 assert MoveSwapClass in Move.__subclasses__() # classmethod style
